10e0618f63049e4002ffd27983ee54cbc539d314
deepakpawade <38484434+deepakpawade@users.noreply.github.com>
2023-02-10 17:13:45 +0530

    Update Training.py

--- Handwritten_Digit_Recognizer/code/Training.py
+++ Handwritten_Digit_Recognizer/code/Training.py
@@ -12,6 +12,7 @@ batch_size = 128
 num_classes = 10
 epochs = 15
 
+# The MNIST dataset contains 28x28 grayscale images. Reshape the training data into 4-dimensional arrays, with the first dimension representing the number of samples (i.e. x_train.shape[0]), the second and third dimensions representing the height and width of the images (i.e. 28, 28), and the fourth dimension representing the number of channels (i.e. 1, since it's a grayscale image)
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
 x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 input_shape = (28, 28, 1)
@@ -20,6 +21,9 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
+#  This line normalizes the training data by dividing each pixel value by 255. 
+#  Normalization helps improve the accuracy of the model by scaling the input data.
+
 x_train /= 255
 x_test /= 255
 print('x_train shape:', x_train.shape)
